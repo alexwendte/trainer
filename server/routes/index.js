c610a11c977dffc73b9e27d35c10ef3259bfd20b
Victor Valdez <victor101185@ksu.edu>
2018-10-20 12:02:31 -0500

    created the mentor list get route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/list', function(req, res, next) {
+  User.find({is_mentor: true}).then((mentors, err) => {
+    if(err) {
+      res.status(404).send({message: 'Could not get any mentors'});
+    }
+
+    res.status(200).send({mentors});
+  }) 
 });
 
 module.exports = router;
